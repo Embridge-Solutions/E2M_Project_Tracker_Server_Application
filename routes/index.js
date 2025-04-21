defcf17572139ae9520324568504c26319790dc1
vijayalakshmi <mailmevijayalakshmi96@gmail.com>
2025-04-21 13:25:02 +0530

    backend setup completed

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,5 @@ const routes = [
   { path: `${prefix}users`, file: 'user' },
   { path: `${prefix}role`, file: 'role' },
   { path: `${prefix}scope`, file: 'scope' },
-  { path: `${prefix}furnace`, file: 'furnace' },
-  { path: `${prefix}pouring`, file: 'pouring' },
-  { path: `${prefix}downTime`, file: 'downTime' },
 ];
 module.exports = routes;
